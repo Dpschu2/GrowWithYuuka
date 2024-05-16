17e56e40a714b679c1929aaf8fa0bb6588e7c0eb
Dean Schulz <dean.schulz@americaneagle.com>
2024-05-16 01:40:09 -0500

    f

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,6 @@ const { merge } = require('webpack-merge'),
       commonConfig = require('./webpack.common.js');
 
 module.exports = merge(commonConfig, {
-    devtool: 'source-map',
     mode: 'production',
     optimization: {
         emitOnErrors: false,
